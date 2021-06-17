7101cba33622482557b767f15f639d9b8412ab55
Meltem Odabas <meltem.odabas.mo@gmail.com>
2021-06-17 12:09:30 -0400

    generate one mode edgelists (in progress)

---
+++ workflow/01-generate-onemode-edgelists.R
@@ -0,0 +1,34 @@
+###
+#
+# Generate the one-mode beer networks
+#
+###
+
+#dtaa locations
+bucket = 'D:\\data\\socialmediadata-beeradvocate'
+loc_import = 'data_bipartite_network'
+loc_export = 'data_onemode_network'
+
+if(dir.exists( file.path(bucket,loc_export) ) == FALSE){dir.create( file.path(bucket,loc_export) )}
+
+#libraries
+library(data.table)
+
+#load data
+weights = c('review_overall',
+            'review_aroma',
+            'review_appearance',
+            'review_palate',
+            'review_taste')
+
+fname = paste0('bipartite_edgelist_', weights[1], '.csv')
+
+df = fread(file.path(bucket,loc_import,fname))
+
+df1 = df[ ,.(beer_name, review_profilename, scale)]
+setnames(df1, c('beer_name', 'scale'), c('beer_name.1', 'scale.1') )
+df2 = df[ ,.(beer_name, review_profilename, scale)]
+setnames(df2, c('beer_name', 'scale'), c('beer_name.2', 'scale.2') )
+rm(df)
+gc()
+result = merge(df1, df2, by = 'review_profilename', allow.cartesian=TRUE)
