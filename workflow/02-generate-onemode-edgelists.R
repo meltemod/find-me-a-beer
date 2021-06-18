d3e8332196517c7e6acde3026829576a2d761e0a
modabas <modabas@iu.edu>
2021-06-18 07:44:46 -0400

    x

--- workflow/02-generate-onemode-edgelists.R
+++ workflow/02-generate-onemode-edgelists.R
@@ -17,6 +17,7 @@ if(dir.exists( file.path(bucket,loc_export) ) == FALSE){dir.create( file.path(bu
 
 #libraries
 library(data.table)
+library(fst)
 
 #load data
 weights = c('review_overall',
@@ -25,15 +26,66 @@ weights = c('review_overall',
             'review_palate',
             'review_taste')
 
-fname = paste0('bipartite_edgelist_', weights[1], '.csv')
 
-df = fread(file.path(bucket,loc_import,fname))
+one_mode_edgelist = function(data,
+                             ego = 'beer_beerid',
+                             alter = 'review_profilename',
+                             weight,
+                             type = c('origin', 'scale', 'scale_byuser')){
+  if(type == 'origin'){
+    n = c(ego, alter, weight)
+  }else{
+    n = c(ego, alter, type)
+  }
+  data = data[ ,..n]
+  gc()
+  result = merge(data, data, by = 'review_profilename', allow.cartesian=TRUE)
+  result = result[ beer_beerid.x < beer_beerid.y, -c('review_profilename')] #eliminate duplicates
+  result
+}
 
-df1 = df[ ,.(beer_name, review_profilename, scale)]
-setnames(df1, c('beer_name', 'scale'), c('beer_name.1', 'scale.1') )
-df2 = df[ ,.(beer_name, review_profilename, scale)]
-setnames(df2, c('beer_name', 'scale'), c('beer_name.2', 'scale.2') )
-rm(df)
-gc()
-result = merge(df1, df2, by = 'review_profilename', allow.cartesian=TRUE)
-result = result[ beer_name.1 != beer_name.2]
+# get_scores = function(result,
+#                              ego = 'beer_beerid',
+#                              alter = 'review_profilename',
+#                              weight,
+#                              type = c('origin', 'scale', 'scale_byuser')){
+#   cname = paste0(type,'_overlap')
+#   n1 = names(result)[2]
+#   n2 = names(result)[4]
+#   print('calculating scores...')
+#   result[ , (cname):= round(eval(parse(text=n1)) * eval(parse(text=n2)),3)]
+#   n = c('beer_beerid.x', 'beer_beerid.y', cname)
+#   result = result[ , ..n]
+#   cname_mean = paste0(cname,'_mean')
+#   cname_sd = paste0(cname,'_sd')
+#   print('calculating counts...')
+#   result[, count := .N, by = c('beer_beerid.x', 'beer_beerid.y')]
+#   print('calculating means...')
+#   result[, (cname_mean) := round(mean(scale_overlap),3), by = c('beer_beerid.x', 'beer_beerid.y')]
+#   print('calculating sds...')
+#   result[, (cname_sd) := round(sd(scale_overlap),3), by = c('beer_beerid.x', 'beer_beerid.y')]
+#   n = c('beer_beerid.x', 'beer_beerid.y', 'count', cname_mean, cname_sd)
+#   result = result[, ..n]
+#   result = unique(result)
+#   result
+# }
+
+
+for(w in weights){
+  print(paste('opening',w ,'data'))
+  fname = paste0('bipartite_edgelist_', w, '.csv')
+  df = fread(file.path(bucket,loc_import,fname))
+  df = data.table(df)
+  df = df[review_profilename != '']
+  for(t in c('origin', 'scale', 'scale_byuser')){
+    print(paste(w,t))
+
+    df_new = one_mode_edgelist(data = df, weight = w, type = t)
+    fname = paste0('onemode_edgelist_', w, "_", t, ".fst")
+    write_fst(df_new, file.path(bucket,loc_export,fname), 100)
+    rm(df_new)
+    gc()
+  }
+  rm(df)
+  gc()
+}
