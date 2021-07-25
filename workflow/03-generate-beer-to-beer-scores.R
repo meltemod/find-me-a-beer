96fa07529691c9b85bc162469c2359627a7975f3
modabas <modabas@iu.edu>
2021-07-25 13:04:11 -0400

    fix bug

--- workflow/03-generate-beer-to-beer-scores.R
+++ workflow/03-generate-beer-to-beer-scores.R
@@ -28,23 +28,39 @@ weights = c('review_overall',
             'review_taste')
 type = c('origin', 'scale', 'scale_byuser')
 
-for (w in weights){
-  for (t in type){
-    fname = paste0('onemode_edgelist_', w, "_", t, ".fst")
-    df = read_fst(file.path(bucket,loc_import,fname))
-    df = as.data.table(df)
-    cname = paste0("score_",str_sub(w,8,nchar(w)),"_",t)
-    sc1 = names(df)[2]
-    sc2 = names(df)[4]
-    df[, score:= sqrt( eval(parse(text = sc1)) * eval(parse(text = sc2)) )]
-    df = df[, c(1,3,5)]
-    key = names(df)[1:2]
-    df = df[, mean(score), by = key]
-    df[, V1 := round(V1,2)]
-    setnames(df, "V1", cname)
-    fname2 = paste0('beer_to_beer_score_', w, "_", t, ".fst")
-    write_fst(df, file.path(bucket,loc_export,fname2), 100)
-    rm(df)
-    gc()
-  }
+
+generate_score = function(w,t){
+  fname = paste0('onemode_edgelist_', w, "_", t, ".fst")
+  df = read_fst(file.path(bucket,loc_import,fname))
+  df = as.data.table(df)
+  cname = paste0("score_",str_sub(w,8,nchar(w)),"_",t)
+  sc1 = names(df)[2]
+  sc2 = names(df)[4]
+  df[, score:= sqrt( eval(parse(text = sc1)) * eval(parse(text = sc2)) )]
+  df = df[, c(1,3,5)]
+  key = names(df)[1:2]
+  df = df[, mean(score), by = key]
+  df[, V1 := round(V1,2)]
+  setnames(df, "V1", cname)
+  fname2 = paste0('beer_to_beer_score_', w, "_", t, ".fst")
+  write_fst(df, file.path(bucket,loc_export,fname2), 100)
+  rm(df)
+  gc()
+  paste("Completed running...", w, t, "...at", Sys.time())
 }
+
+generate_score(w = weights[1], t = type[1]) #running
+generate_score(w = weights[2], t = type[1]) #running
+generate_score(w = weights[3], t = type[1]) #running
+generate_score(w = weights[4], t = type[1]) #running
+generate_score(w = weights[5], t = type[1]) #running
+generate_score(w = weights[1], t = type[2])
+generate_score(w = weights[2], t = type[2])
+generate_score(w = weights[3], t = type[2])
+generate_score(w = weights[4], t = type[2])
+generate_score(w = weights[5], t = type[2])
+generate_score(w = weights[1], t = type[3])
+generate_score(w = weights[2], t = type[3])
+generate_score(w = weights[3], t = type[3])
+generate_score(w = weights[4], t = type[3])
+generate_score(w = weights[5], t = type[3])
